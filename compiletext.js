0d41da230fd4b03136ee6fe7943d4aef533fb935
Pierce Stegman <pwstegman@gmail.com>
2015-01-17 00:17:59 -0500

    Speech and basic string parsing

---
+++ compiletext.js
@@ -0,0 +1,48 @@
+var functions = {};
+
+function processString(str){
+	str = str.split(" ");
+	if(str.indexOf("param") != -1 && str.indexOf("function") != -1){
+		addParameter(str[str.indexOf("function")+1],str[str.indexOf("param")+1])
+		return;
+	}
+	if(str.indexOf("define") != -1){
+		addFunction(str[str.indexOf("define")+1]);
+		return;
+	}
+	if(str.indexOf("function") != -1 && str.indexOf("return") != -1){
+		addReturn(str[str.indexOf("function")+1], str[str.indexOf("return")+1]);
+	}
+}
+
+function addFunction(name){
+	functions[name] = {params:[],lines:[],re:""};
+}
+
+function addParameter(func,name){
+	functions[func]["params"].push(name);
+}
+
+function addLine(func,line){
+	functions[func]["lines"].push(line);
+}
+
+function addReturn(func,line){
+	functions[func]["re"] = line;
+}
+
+function compile(){
+	var result = "";
+	for (var key in functions) {
+	  if (functions.hasOwnProperty(key)) {
+	    result += "def "+key+"("+functions[key].params.join(", ")+"):\n";
+	    for(var i=0;i<functions[key].lines.length;i++){
+	    	result += "  "+functions[key].lines[i]+"\n";
+	    }
+	    if(functions[key].re != ""){
+	    	result += "  return "+functions[key].re+"\n";
+	    }
+	  }
+	}
+	return result;
+}
