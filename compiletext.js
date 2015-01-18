ccaa66c96f561fbd0dfe9e20781533b686540dfb
Pierce Stegman <pwstegman@gmail.com>
2015-01-18 02:09:27 -0500

    Improved text option

--- compiletext.js
+++ compiletext.js
@@ -42,7 +42,8 @@ function processString(str){
 
 	 	while(str.indexOf("text") != -1){
 	 		str[str.indexOf("text")+1] = '"'+str.slice(str.indexOf("text")+1).join(" ")+'"';
-	 		str.splice(str.indexOf("text"));
+	 		str.splice(str.indexOf("text"),1);
+	 		str.splice(str.indexOf("text")+2);
 	 	}
 
 	 	if(str.indexOf("loop") != -1 && str.indexOf("line") != -1){
